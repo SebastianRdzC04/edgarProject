5b2d776c4875bd0402be85e6166ed759ad7885dd
SebastianRdzC04 <22030138@uttcampus.edu.mx>
2025-05-30 20:28:25 -0600

    ahora si ya va a jalar

--- edgar-fron/src/components/react_components/QuotesTable.tsx
+++ edgar-fron/src/components/react_components/QuotesTable.tsx
@@ -96,12 +96,12 @@ const QuotesTable: React.FC = () => {
           <p className="mt-2 text-gray-300">Lista de cotizaciones de servicios de construcción.</p>
         </div>
         <div>
-          <button className="px-4 py-2 bg-blue-300 text-black font-bold rounded-lg hover:bg-blue-400 transition duration-300 flex items-center shadow-sm">
+          <a href='/create-quotes' className="px-4 py-2 bg-blue-300 text-black font-bold rounded-lg hover:bg-blue-400 transition duration-300 flex items-center shadow-sm">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
             </svg>
             Nueva Cotización
-          </button>
+          </a>
         </div>
       </div>
 
